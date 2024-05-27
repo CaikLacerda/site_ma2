acf89f51eac39c8a24bd523faace04bc71f8ad6a
Caik D. Lacerda <caiklacerda9@gmail.com>
2024-05-26 21:40:33 -0300

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function exibirPodioBasquete() {
+    window.alert('1º Lugar: Educação Física\n2º Lugar: Engenharia\n3º lugar: Sistemas de Informação');
+}
+
+function exibirPodioVolei() {
+    window.alert('1º Lugar: Sistemas de Informação\n2º Lugar: Farmácia\n3º lugar: Educação Física');
+}
+
+function exibirPodioTenis() {
+    window.alert('1º Lugar: Engenhearia da Computação\n2º Lugar: Psicologia\n3º lugar: Sistemas de Informação');
+}
+
+function exibirPodioFut() {
+    window.alert('1º Lugar: Fisioterapia\n2º Lugar: Administração\n3º lugar: Educação Física');
+}
